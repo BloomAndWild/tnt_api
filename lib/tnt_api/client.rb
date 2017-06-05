841a0461e945685b649cbbbbaa4d408632b18516
Gediminas <gediminas.zubovicius@gmail.com>
2017-06-05 15:48:52 +0100

    fix alignment

--- lib/tnt_api/client.rb
+++ lib/tnt_api/client.rb
@@ -1,10 +1,10 @@
 module TNTApi
  class Client
-    class << self
-      attr_reader :config, :errors
+   class << self
+     attr_reader :config, :errors
 
-      def configure
-        raise ArgumentError, "block not given" unless block_given?
+     def configure
+       raise ArgumentError, "block not given" unless block_given?
 
         @config = Config.new
         yield config
