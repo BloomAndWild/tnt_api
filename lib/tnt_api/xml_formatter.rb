a194ad2045ee01e5b5946edb53569c42dbc5a11c
Srikanth Kunkulagunta <srikanth.kunkulagunta@gmail.com>
2017-06-06 16:05:15 +0100

    just use 'to_s' on passed in dates.

--- lib/tnt_api/xml_formatter.rb
+++ lib/tnt_api/xml_formatter.rb
@@ -15,8 +15,6 @@ module TNTApi
     end
 
     def format_attrs(attrs)
-      attrs.merge!(shipping_date: formatted_date(attrs[:shipping_date]))
-
       attrs.each do |k, v|
         next unless v.is_a?(String)
 
@@ -25,18 +23,11 @@ module TNTApi
         )
       end
 
+      attrs[:shipping_date] = attrs[:shipping_date].to_s
       attrs
     end
 
     private
-
-    def formatted_date(shipping_date)
-      if shipping_date.is_a?(Date)
-        shipping_date.to_s
-      else
-        shipping_date
-      end
-    end
 
     def escape_xml_chars(str)
       return str unless str.is_a? String
