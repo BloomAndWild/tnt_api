3538eabf234fd10d078f5eb6b1f302f453bedddc
Chris Guy <chris@bloomandwild.com>
2017-06-07 16:51:39 +0100

    - encode unauthorised characters to decimal HTML entities

--- lib/tnt_api/xml_formatter.rb
+++ lib/tnt_api/xml_formatter.rb
@@ -8,6 +8,8 @@ module TNTApi
       ">" => "&gt;",
     }.freeze
 
+    UNAUTHORIZED_CHARACTERS_REGEX = /[^A-Z0-9àâäéèêëîïôöùûüñÀÂÄÉÈÊËÎÏÔÖÙÛÜÑ 2&"#'{}()\[\]|_\\\/Çç^@°=+\-£$¤%μ*<>?,\.;:§!]/i.freeze
+
     class << self
       def format_attrs(attrs)
         new.format_attrs(attrs)
@@ -16,18 +18,35 @@ module TNTApi
 
     def format_attrs(attrs)
       attrs.each do |k, v|
-        next unless v.is_a?(String)
-        attrs[k] = escape_xml_chars(v.unicode_normalize)
+        case v.class.name
+        when "String"
+          attrs[k] = format_string(v)
+        when "Date"
+          attrs[k] = v.to_s
+        end
       end
 
-      attrs[:shipping_date] = attrs[:shipping_date].to_s
       attrs
     end
 
     private
 
-    def escape_xml_chars(str)
+    def format_string(str)
       return str unless str.is_a? String
+      escape_xml_chars(
+          encode_unauthorized_chars(
+            str.unicode_normalize
+          )
+        )
+    end
+
+    def encode_unauthorized_chars(str)
+      str.gsub(UNAUTHORIZED_CHARACTERS_REGEX) do |v|
+        "&##{v.ord};"
+      end
+    end
+
+    def escape_xml_chars(str)
       str.gsub(/["&'<>]/, XML_SPECIAL_CHARACTER_MAP)
     end
 
